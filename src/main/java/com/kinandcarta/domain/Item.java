01667bd136e57c96c7b17aa06cf58381380a04b9
Mabon ap Gwyn <mabon@mabon-ap-gwyn.home>
2022-01-11 14:29:14 +0000

    added instance vars and getter methods to Item.java

--- src/main/java/com/kinandcarta/domain/Item.java
+++ src/main/java/com/kinandcarta/domain/Item.java
@@ -6,12 +6,25 @@ package com.kinandcarta.domain;
 public class Item {
 
     // Define a private field for the item id.
-    
+    private int id;
     // Define a private field for the item description.
-
+    private String description;
     // Define a private field for the price.
-
+    private int price;
     // Define a constructor which will take values for all three fields as parameters and assign them to the fields.
-
+    public Item(int id, String description, int price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
     // Add 'get' methods that will allow external access to the fields.
+    public int getId() {
+        return this.id;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public int getPrice() {
+        return this.price;
+    }
 }
